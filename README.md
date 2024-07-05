08bde0a99d8bc5f4548bf3eef9d0c0f7ffe0c59b
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-07-05 13:15:00 +0200

    fix: https

---
+++ README.md
@@ -0,0 +1,5 @@
+## Development
+
+1. run `npm ci`
+2. run `npm start`
+3. open https://localhost:3000/
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
+  plugins: [mkcert()],
   server: {
     open: true,
+    port: 3000,
+    https: true,
   },
 });
