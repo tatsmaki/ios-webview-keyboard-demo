54c94f82ff658d13fefe3f289f43555dfbf9c750
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2024-07-05 14:00:00 +0200

    test: opacity & position

--- src/dialog.ts
+++ src/dialog.ts
@@ -1,11 +1,11 @@
 const app = document.getElementById("app")!;
 const dialog = document.createElement("dialog");
-const input = document.createElement("input");
+const dialogInput = document.createElement("input");
 const close = document.createElement("button");
 
-input.placeholder = "Input";
+dialogInput.placeholder = "Input";
 close.textContent = "Close";
-dialog.append(input, close);
+dialog.append(dialogInput, close);
 
 export const openDialog = () => {
   app.append(dialog);
@@ -16,3 +16,29 @@ export const closeDialog = () => {
 };
 
 close.onclick = closeDialog;
+
+/* Opacity hack */
+
+dialogInput.onfocus = () => {
+  dialogInput.style.opacity = "0";
+  setTimeout(() => (dialogInput.style.opacity = "1"), 100);
+};
+
+/* Visual viewport test */
+
+const initialHeight = window.innerHeight;
+
+visualViewport?.addEventListener("resize", () => {
+  const viewportHeight = visualViewport?.height || 0;
+  const isKeyboardOpen = viewportHeight < initialHeight;
+
+  if (isKeyboardOpen) {
+    // const { bottom } = dialogInput.getBoundingClientRect();
+    // const padding = 8;
+    // dialog.style.top = `calc(50% - ${bottom - viewportHeight / 2}px)`;
+    // dialogInput.value = String(dialog.style.top);
+    // dialogInput.value = String(bottom);
+  } else {
+    // dialog.style.top = "50%";
+  }
+});
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { openDialog } from "./dialog";
 import "./style.css";
 
 const dialogButton = document.getElementById("open-dialog")!;
-// const viewportText = document.getElementById("viewport")!;
 
 dialogButton.onclick = openDialog;
 
-visualViewport?.addEventListener("resize", () => {
-  console.log(visualViewport?.height);
-  //   viewportText.textContent = String(
-  //     visualViewport?.height || window.innerHeight
-  //   );
-});
+/* Opacity hack */
+
+const pageInput = document.getElementById("page-input")!;
+
+pageInput.onfocus = () => {
+  pageInput.style.opacity = "0";
+  setTimeout(() => (pageInput.style.opacity = "1"), 100);
+};
